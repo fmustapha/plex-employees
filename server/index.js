76a3831159bae9528a08dc7f032b8aa0410066cf
Faith Mustapha <f81@MacBook-Air.phub.net.cable.rogers.com>
2020-04-07 23:48:37 -0400

    make port dynamic and add delete route

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const employees = require("./data/employees.json");
 
 app.use(express.json());
 
+const port = process.env.PORT || 8080;
+
 var corsOptions = {
   origin: "http://localhost:3000",
   optionsSuccessStatus: 200,
@@ -14,6 +16,26 @@ app.get("/api/employees", cors(corsOptions), (req, res, next) => {
   res.setHeader("Content-Type", "application/json");
   res.status(200);
   res.send(JSON.stringify(employees, null, 2));
+});
+
+app.post("/api/employees", cors(corsOptions), (req, res, next) => {
+  //Validate the req.body
+  // return 400 - bad request if invalid
+  const { name } = req.body;
+  if (employees.find((e) => e.name === name)) {
+    return res.status(400).send("The employee already exists");
+  }
+
+  //Check if the employee already exists by checking the name
+  //If true return 300 already exists . error
+
+  // push the new employee data into the existing employees array
+  console.log(req.body);
+  let id = employees.length;
+  id += 1;
+  const employee = { id, ...req.body };
+  employees.push(employee);
+  res.status(200).send(employees);
 });
 
 app.put("/api/employees/:id", cors(corsOptions), (req, res, next) => {
@@ -38,11 +60,21 @@ app.put("/api/employees/:id", cors(corsOptions), (req, res, next) => {
   res.send(employee);
 });
 
+app.delete("/api/employees/:id", (req, res) => {
+  const employee = employees.find((e) => e.id === parseInt(req.params.id));
+  if (!employee) return res.status(404).send("Employee does not exist");
+
+  const index = employees.indexOf(employee);
+  employees.splice(index, 1);
+  res.send(employees);
+});
+
 function validateEmployee(data) {
-  if (data.name.length >= 5 && data.name.length <= 20) {
+  const { name } = data;
+  if (name.length >= 5 && name.length <= 20) {
     return true;
   }
   return false;
 }
 
-app.listen(8080, () => console.log("Job Dispatch API running on port 8080!"));
+app.listen(port, () => console.log(`Job Dispatch API running on port ${port}!`));
