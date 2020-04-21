1ba6e8ff37db03b908883f95a9ed260c9d54f800
Faith Mustapha <f81@MacBook-Air.phub.net.cable.rogers.com>
2020-04-20 23:54:05 -0400

    update fetch endpoint in App

--- src/App.js
+++ src/App.js
@@ -1,40 +1,33 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
   state = {
-    employees: []
-  }
-  
+    employees: [],
+  };
+
   componentWillMount = () => {
-    fetch('http://localhost:8080/api/employees')
-      .then(response => response.json())
-      .then(employees => this.setState({ employees }))
-  }
+    fetch("http://localhost:8080/")
+      .then((response) => response.json())
+      .then((employees) => this.setState({ employees }));
+  };
 
   render() {
-    const {
-      employees
-    } = this.state;
+    const { employees } = this.state;
 
     console.log(this.state);
 
     return (
       <div className="App">
         <h1>Plexxis Employees</h1>
-        {
-          employees.map(employee => (
-            <div key={employee.id}>
-              {
-                Object.keys(employee).map(key => 
-                  <span key={key}>
-                    { key }:
-                    { employee[key] } 
-                  </span>
-                )
-              }
-            </div>
-          ))
-        }
+        {employees.map((employee) => (
+          <div key={employee.id}>
+            {Object.keys(employee).map((key) => (
+              <span key={key}>
+                {key}:{employee[key]}
+              </span>
+            ))}
+          </div>
+        ))}
       </div>
     );
   }
